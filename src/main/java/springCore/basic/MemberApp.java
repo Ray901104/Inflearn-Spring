2c9f3b55c554c4889ccef39bd202adc247863720
Ray901104 <ip323@naver.com>
2023-10-09 22:14:56 +0900

    회원 도메인 실행과 테스트

---
+++ src/main/java/springCore/basic/MemberApp.java
@@ -0,0 +1,19 @@
+package springCore.basic;
+
+import springCore.basic.member.Grade;
+import springCore.basic.member.Member;
+import springCore.basic.member.MemberService;
+import springCore.basic.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
