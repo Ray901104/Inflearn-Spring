941a52f47f8b5dd72678dd1d7cc034ff3fd2e00f
Ray901104 <ip323@naver.com>
2023-10-09 13:28:22 +0900

    JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,13 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 알아서 생성해주는 것 IDENTITY
     private Long id; // 데이터를 구분하기 위해 시스템이 정하는 아이디
+
     private String name;
 
     public Long getId() {
