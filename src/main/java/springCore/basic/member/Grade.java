64bcdc0d66ef6b0afc7eff77b483b6125b0fadd8
Ray901104 <ip323@naver.com>
2023-10-09 22:04:39 +0900

    회원 도메인 개발

---
+++ src/main/java/springCore/basic/member/Grade.java
@@ -0,0 +1,6 @@
+package springCore.basic.member;
+
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ src/main/java/springCore/basic/member/MemberService.java
@@ -0,0 +1,8 @@
+package springCore.basic.member;
+
+public interface MemberService {
+
+    void join(Member member);
+
+    Member findMember(Long memberId);
+}
