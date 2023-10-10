d3c2c3badd3f9413218f7e64a29cbcd38df8519b
Ray901104 <ip323@naver.com>
2023-10-10 11:23:52 +0900

    AppConfig 리팩토링

--- src/main/java/springCore/basic/AppConfig.java
+++ src/main/java/springCore/basic/AppConfig.java
@@ -1,6 +1,8 @@
 package springCore.basic;
 
+import springCore.basic.discount.DiscountPolicy;
 import springCore.basic.discount.FixDiscountPolicy;
+import springCore.basic.member.MemberRepository;
 import springCore.basic.member.MemberService;
 import springCore.basic.member.MemberServiceImpl;
 import springCore.basic.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import springCore.basic.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
