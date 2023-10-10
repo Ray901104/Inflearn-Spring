553bc8fced19525f056fc962dfd72b13af7ef601
Ray901104 <ip323@naver.com>
2023-10-10 09:02:42 +0900

    주문과 할인 도메인 실행과 테스트

---
+++ src/test/java/springCore/basic/order/OrderServiceTest.java
@@ -0,0 +1,29 @@
+package springCore.basic.order;
+
+import org.junit.jupiter.api.Test;
+import springCore.basic.member.Grade;
+import springCore.basic.member.Member;
+import springCore.basic.member.MemberService;
+import springCore.basic.member.MemberServiceImpl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        // given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        // when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // then
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
