8520cc24215380b579f33dd08474ab5799e49060
Ray901104 <ip323@naver.com>
2023-10-13 23:12:23 +0900

    필터

---
+++ src/test/java/springCore/basic/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,9 @@
+package springCore.basic.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+}
