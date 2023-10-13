0f41de08e89bf2f2a027894f3e1f4985fe4dc6ff
Ray901104 <ip323@naver.com>
2023-10-13 23:03:51 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/springCore/basic/AutoAppConfig.java
+++ src/main/java/springCore/basic/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "springCore.basic", // default : AutoAppConfig의 패키지 하위를 모두 스캔한다. 보통 프로젝트의 시작 위치에 지정한다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
