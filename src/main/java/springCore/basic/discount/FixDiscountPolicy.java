684ba9db95bb06eff01062e8f446cfa54e38b7ed
Ray901104 <ip323@naver.com>
2023-10-14 22:42:28 +0900

    문제 상황 1. OrderService 를 스프링이 빈으로 등록할 때 DiscountPolicy 를 DI 해준다. 2. DiscountPolicy 타입의 빈이 2개 존재한다. (FixDiscountPolicy, RateDiscountPolicy) 3. 어떤 빈을 OrderService 에 DI 해줄지 스프링으로서는 알 수가 없어 에러가 발생한다.

--- src/main/java/springCore/basic/discount/FixDiscountPolicy.java
+++ src/main/java/springCore/basic/discount/FixDiscountPolicy.java
@@ -1,8 +1,10 @@
 package springCore.basic.discount;
 
+import org.springframework.stereotype.Component;
 import springCore.basic.member.Grade;
 import springCore.basic.member.Member;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000;
