d256cd4fee0b3fda6c92f4a9c5272b20e1dcf3eb
Ray901104 <ip323@naver.com>
2023-10-15 21:39:06 +0900

    프로토타입 빈을 싱글톤 빈과 함께 사용할 때 문제 해결 - ObjectProvider 사용

--- src/test/java/springCore/basic/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/springCore/basic/scope/SingletonWithPrototypeTest1.java
@@ -3,6 +3,7 @@ package springCore.basic.scope;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -35,19 +36,20 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입 -> 계속 같은 것을 사용
+        private final ObjectProvider<PrototypeBean> prototypeBeanObjectProvider; // DL 을 도와주는 기능
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanObjectProvider) {
+            this.prototypeBeanObjectProvider = prototypeBeanObjectProvider;
         }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanObjectProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
