14739976717066034357e51aa93c4467689ff66d
Ray901104 <ip323@naver.com>
2023-10-15 22:30:23 +0900

    request 스코프와 Provider

--- src/main/java/springCore/basic/web/LogDemoController.java
+++ src/main/java/springCore/basic/web/LogDemoController.java
@@ -2,6 +2,7 @@ package springCore.basic.web;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,11 +13,13 @@ import springCore.basic.common.MyLogger;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody // 문자를 그대로 반환할 수 있다.
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
+
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
