cf63cbdb14bd005b93c321d0c888021c6d18d9d0
yrcho <ip323@naver.com>
2023-11-06 20:15:54 +0900

    Validator 분리 - 2

--- SpringMVC-Validation/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ SpringMVC-Validation/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -5,9 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ItemServiceApplication {
+//public class ItemServiceApplication implements WebMvcConfigurer { // WebDataBinder 글로벌 설정방법
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+	// WebDataBinder 글로벌 설정방법
+//	@Override
+//	public Validator getValidator() {
+//		return new ItemValidator();
+//	}
 }
