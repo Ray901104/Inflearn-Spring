fcec5871e75e6fbe96e6ab790731ad5195c74a6f
yrcho <ip323@naver.com>
2023-11-12 11:05:55 +0900

    로그인 처리하기 - 서블릿 HTTP 세션2 (스프링 사용)

--- SpringMVC-Login/src/main/java/hello/login/web/login/LoginController.java
+++ SpringMVC-Login/src/main/java/hello/login/web/login/LoginController.java
@@ -92,8 +92,6 @@ public class LoginController {
         return "redirect:/";
     }
 
-
-
     //    @PostMapping("/logout")
     public String logout(HttpServletResponse response) {
         expireCookie(response, "memberId");
