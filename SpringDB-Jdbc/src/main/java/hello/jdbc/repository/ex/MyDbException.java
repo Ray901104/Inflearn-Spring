c3c3f0a6c1ccbd96fbfaec9c404940cdb4a6d730
조영래 <ip323@naver.com>
2023-11-24 10:59:29 +0900

    데이터 접근 예외 직접 만들기

--- SpringDB-Jdbc/src/main/java/hello/jdbc/repository/ex/MyDbException.java
+++ SpringDB-Jdbc/src/main/java/hello/jdbc/repository/ex/MyDbException.java
@@ -4,6 +4,10 @@ public class MyDbException extends RuntimeException {
     public MyDbException() {
     }
 
+    public MyDbException(String message) {
+        super(message);
+    }
+
     public MyDbException(String message, Throwable cause) {
         super(message, cause);
     }
