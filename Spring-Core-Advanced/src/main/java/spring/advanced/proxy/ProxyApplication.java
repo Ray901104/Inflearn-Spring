85bf4d82a0091364df727746280db1f947a671df
조영래 <ip323@naver.com>
2023-12-21 11:32:27 +0900

    프록시 예제 프로젝트 만들기 v2

--- Spring-Core-Advanced/src/main/java/spring/advanced/proxy/ProxyApplication.java
+++ Spring-Core-Advanced/src/main/java/spring/advanced/proxy/ProxyApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import spring.advanced.proxy.config.AppV1Config;
+import spring.advanced.proxy.config.AppV2Config;
 
-@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "spring.advanced.proxy.app.v3")
 public class ProxyApplication {
 
