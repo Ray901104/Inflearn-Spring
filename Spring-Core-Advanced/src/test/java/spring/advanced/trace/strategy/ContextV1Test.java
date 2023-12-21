bb7061927e1931152ea3cc69a5593e999d406181
조영래 <ip323@naver.com>
2023-12-21 09:12:59 +0900

    전략 패턴 - 예제2 고려 사항: 선조립 후실행의 단점

--- Spring-Core-Advanced/src/test/java/spring/advanced/trace/strategy/ContextV1Test.java
+++ Spring-Core-Advanced/src/test/java/spring/advanced/trace/strategy/ContextV1Test.java
@@ -3,6 +3,7 @@ package spring.advanced.trace.strategy;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import spring.advanced.trace.strategy.code.strategy.ContextV1;
+import spring.advanced.trace.strategy.code.strategy.Strategy;
 import spring.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import spring.advanced.trace.strategy.code.strategy.StrategyLogic2;
 
@@ -46,6 +47,68 @@ public class ContextV1Test {
 
         StrategyLogic2 strategyLogic2 = new StrategyLogic2();
         ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스 v1
+     */
+    @Test
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        log.info("strategyLogic1={}", strategyLogic1.getClass());
+        contextV1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        log.info("strategyLogic2={}", strategyLogic2.getClass());
+        contextV2.execute();
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스 v2
+     */
+    @Test
+    void strategyV3() {
+        ContextV1 contextV1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+        contextV2.execute();
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스 v3
+     */
+    @Test
+    void strategyV4() {
+        ContextV1 contextV1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
         contextV2.execute();
     }
 }
