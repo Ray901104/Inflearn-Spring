c8a2852a2fd70d55f680fa96738c59521ce5ccec
조영래 <ip323@naver.com>
2023-12-21 12:35:42 +0900

    프록시 패턴 - 예제 코드2 (접근제어)

--- Spring-Core-Advanced/src/test/java/spring/advanced/proxy/pureproxy/ProxyPatternTest.java
+++ Spring-Core-Advanced/src/test/java/spring/advanced/proxy/pureproxy/ProxyPatternTest.java
@@ -1,6 +1,7 @@
 package spring.advanced.proxy.pureproxy;
 
 import org.junit.jupiter.api.Test;
+import spring.advanced.proxy.pureproxy.code.CacheProxy;
 import spring.advanced.proxy.pureproxy.code.ProxyPatternClient;
 import spring.advanced.proxy.pureproxy.code.RealSubject;
 
@@ -10,6 +11,16 @@ public class ProxyPatternTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
