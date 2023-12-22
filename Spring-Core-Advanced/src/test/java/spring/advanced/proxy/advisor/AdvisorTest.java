9a7ee1fa656ebbdde302bcac17d9b202301fa05c
조영래 <ip323@naver.com>
2023-12-22 10:34:21 +0900

    예제 코드1 - 어드바이저

---
+++ Spring-Core-Advanced/src/test/java/spring/advanced/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,24 @@
+package spring.advanced.proxy.advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import spring.advanced.proxy.common.advice.TimeAdvice;
+import spring.advanced.proxy.common.service.ServiceImpl;
+import spring.advanced.proxy.common.service.ServiceInterface;
+
+public class AdvisorTest {
+
+    @Test
+    void advisorTest1() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+}
