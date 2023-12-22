b74823540bcc7dd20c734ca41236698138329eda
조영래 <ip323@naver.com>
2023-12-22 12:52:19 +0900

    예제 코드3 - 스프링이 제공하는 포인트컷

--- Spring-Core-Advanced/src/test/java/spring/advanced/proxy/advisor/AdvisorTest.java
+++ Spring-Core-Advanced/src/test/java/spring/advanced/proxy/advisor/AdvisorTest.java
@@ -8,6 +8,7 @@ import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 import spring.advanced.proxy.common.advice.TimeAdvice;
 import spring.advanced.proxy.common.service.ServiceImpl;
 import spring.advanced.proxy.common.service.ServiceInterface;
@@ -36,6 +37,24 @@ public class AdvisorTest {
         ProxyFactory proxyFactory = new ProxyFactory(target);
         DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointCut(), new TimeAdvice());
         proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    @Test
+    @DisplayName("스프링이 제공하는 포인트컷")
+    void advisorTest3() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("save");
+
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+
         ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
 
         proxy.save();
