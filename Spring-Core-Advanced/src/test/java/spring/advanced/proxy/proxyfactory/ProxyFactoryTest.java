9b0c5a89bb1ee126ec857d05f56f4476ededa576
조영래 <ip323@naver.com>
2023-12-22 09:43:07 +0900

    프록시 팩토리 - 예제 코드1

---
+++ Spring-Core-Advanced/src/test/java/spring/advanced/proxy/proxyfactory/ProxyFactoryTest.java
@@ -0,0 +1,35 @@
+package spring.advanced.proxy.proxyfactory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+import spring.advanced.proxy.common.advice.TimeAdvice;
+import spring.advanced.proxy.common.service.ServiceImpl;
+import spring.advanced.proxy.common.service.ServiceInterface;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class ProxyFactoryTest {
+
+    @Test
+    @DisplayName("인터페이스가 있으면 JDK 동적 프록시 사용")
+    void interfaceProxy() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice()); //프록시가 제공하는 부가 기능 로직 -> Advice
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.save();
+        proxy.find();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+}
