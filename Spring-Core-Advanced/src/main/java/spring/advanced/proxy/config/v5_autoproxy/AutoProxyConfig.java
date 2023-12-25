6b0273c248727f9bac4fa8442f44d3749155ef06
Youngrae Cho <ip323@naver.com>
2023-12-25 21:32:31 +0900

    스프링이 제공하는 빈 후처리기2

--- Spring-Core-Advanced/src/main/java/spring/advanced/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ Spring-Core-Advanced/src/main/java/spring/advanced/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -1,6 +1,7 @@
 package spring.advanced.proxy.config.v5_autoproxy;
 
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +16,31 @@ import spring.advanced.proxy.trace.logtrace.LogTrace;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-    @Bean
+    //@Bean
     public Advisor advisor1(LogTrace logTrace) {
         //pointcut
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("request*", "order*", "save*");
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    //@Bean
+    public Advisor advisor2(LogTrace logTrace) {
+        //pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* spring.advanced.proxy.app..*(..))");
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    @Bean
+    public Advisor advisor3(LogTrace logTrace) {
+        //pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* spring.advanced.proxy.app..*(..)) && !execution(* spring.advanced.proxy.app..noLog(..))");
         //advice
         LogTraceAdvice advice = new LogTraceAdvice(logTrace);
         return new DefaultPointcutAdvisor(pointcut, advice);
