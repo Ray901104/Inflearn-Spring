03ebab61eb5c1891232b2fcccd230c1344bd5403
조영래 <ip323@naver.com>
2023-12-26 14:39:40 +0900

    재시도 AOP

--- Spring-Aop/src/main/java/spring/aop/example/ExamRepository.java
+++ Spring-Aop/src/main/java/spring/aop/example/ExamRepository.java
@@ -1,6 +1,7 @@
 package spring.aop.example;
 
 import org.springframework.stereotype.Repository;
+import spring.aop.example.annotation.Retry;
 import spring.aop.example.annotation.Trace;
 
 @Repository
@@ -12,6 +13,7 @@ public class ExamRepository {
      * 5번에 1번 실패하는 요청
      */
     @Trace
+    @Retry(4)
     public String save(String itemId) {
         seq++;
         if (seq % 5 == 0) {
